f8787790fb7cd49616b41ed80b6ad3491384da29
Camilo Lima-Castro <crl3554@ad.rit.edu>
2020-10-19 10:26:33 -0400

    Created average value circle

--- src/visualizer.js
+++ src/visualizer.js
@@ -66,59 +66,9 @@ const draw = (params = {}) => {
     if (points.length > 3)
         ctxUtil.fillPolygon(ctx, points, "blue");
 
-    // Draw bars
-    if (params.showBars) {
-        let barSpacing = 4;
-        let margin = 5;
-        let screenWidthForBars = canvasWidth - (audioData.length * barSpacing) - margin * 2;
-        let barWidth = screenWidthForBars / audioData.length;
-        let barHeight = 200;
-        let topSpacing = 100;
-
-        ctx.save();
-        ctx.fillStyle = 'rgba(255, 255, 255, .5)';
-        // loop through the data and draw!
-        for (let i = 0; i < audioData.length; i++) {
-            ctx.fillRect(margin + i * (barWidth + barSpacing), topSpacing + 256 - audioData[i], barWidth, barHeight);
-            ctx.strokeRect(margin + i * (barWidth + barSpacing), topSpacing + 256 - audioData[i], barWidth, barHeight);
-        }
-        ctx.restore();
-    }
-
-    // 5 - draw circles
-    if (params.showCircles) {
-        let maxRadius = canvasHeight / 4;
-        ctx.save();
-        ctx.globalAlpha = .5;
-        for (let i = 0; i < audioData.length; i++) {
-            // red-ish circles
-            let percent = audioData[i] / 255;
-
-            let circleRadius = percent * maxRadius;
-            ctx.beginPath();
-            ctx.fillStyle = `rgba(255, 111, 111, ${.34 - percent / 3.0})`;
-            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-
-            // blue-ish circles, bigger, more transparent
-            ctx.beginPath();
-            ctx.fillStyle = `rgba(0, 0, 255, ${.1 - percent / 10.0})`;
-            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius * 1.5, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-
-            // yellow-ish circles, smaller
-            ctx.save();
-            ctx.beginPath();
-            ctx.fillStyle = `rgba(200, 200, 0, ${.5 - percent / 5.0})`;
-            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius * .5, 0, 2 * Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-            ctx.restore();
-        }
-        ctx.restore();
-    }
+    // Draw average loudness circle
+    let average = mathUtil.average(...audioData);
+    ctxUtil.fillCircle(ctx, centerX, centerY, average * 0.3, "darkblue");
 
     // Bitmap manipulation (only if an)
     let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
